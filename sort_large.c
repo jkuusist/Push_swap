596af999d6855b2e71c6bd774383443a6b39f6fa
jkuusist <jkuusist@student.hive.fi>
2020-08-17 15:28:35 +0200

    norme fixes for sort_large

--- sort_large.c
+++ sort_large.c
@@ -13,12 +13,17 @@
 #include "includes/push_swap.h"
 #include "libft/libft.h"
 
-void	sort_large(t_stack *a, t_stack *b)
+static	void	push_or_rot(t_stack *a, t_stack *b)
 {
-	int i;
+	if ((stack_peek(a) < a->second_quart)
+			&& (stack_peek(a) >= a->first_quart))
+		push_ab(a, b, 'b', 1);
+	else
+		rotate_ab(a, 'a', 1);
+}
 
-	i = 0;
-	get_quarts(a);
+static	void	do_first_two(t_stack *a, t_stack *b, int i)
+{
 	while (i < (int)a->size)
 	{
 		if (stack_peek(a) >= a->third_quart)
@@ -42,19 +47,17 @@ void	sort_large(t_stack *a, t_stack *b)
 			rotate_ab(a, 'a', 1);
 		i++;
 	}
-	b->smallest = b->arr[get_smallest(b)];
-	b->largest = b->arr[get_largest(b)];
+}
+
+static	void	do_next_two(t_stack *a, t_stack *b, int i)
+{
 	while (b->top > 0)
 		do_least_moves(a, b);
 	push_ab(a, b, 'a', 1);
 	i = 0;
 	while (i < (int)a->size)
 	{
-		if ((stack_peek(a) < a->second_quart)
-			&& (stack_peek(a) >= a->first_quart))
-			push_ab(a, b, 'b', 1);
-		else
-			rotate_ab(a, 'a', 1);
+		push_or_rot(a, b);
 		i++;
 	}
 	b->smallest = b->arr[get_smallest(b)];
@@ -71,6 +74,19 @@ void	sort_large(t_stack *a, t_stack *b)
 			rotate_ab(a, 'a', 1);
 		i++;
 	}
+}
+
+void			sort_large(t_stack *a, t_stack *b)
+{
+	int i;
+
+	i = 0;
+	get_quarts(a);
+	do_first_two(a, b, i);
+	b->smallest = b->arr[get_smallest(b)];
+	b->largest = b->arr[get_largest(b)];
+	i = 0;
+	do_next_two(a, b, i);
 	b->smallest = b->arr[get_smallest(b)];
 	b->largest = b->arr[get_largest(b)];
 	while (b->top > 0)
