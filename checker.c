5be6a6a799766f85962ca7314608fb14612cbc8e
jkuusist <jkuusist@student.hive.fi>
2020-08-17 10:03:00 +0200

    fixed issue where checker was segfaulting when run without any arguments due to the -v check being outside the check for whether arguments exist

--- checker.c
+++ checker.c
@@ -22,41 +22,27 @@ int		main(int argc, char **argv)
 	t_stack *b;
 	int		i;
 
-	if (!ft_strcmp(argv[1], "-v"))
+/*	if (!ft_strcmp(argv[1], "-v"))
 	{
 		checker_print(argc, argv);
 		return (0);
-	}
-	else if (argc > 1)
+	}*/
+/*	else */ if (argc > 1)
 	{
-		if (((argc <= 2) && (ft_strchr(argv[1], ' ')))) //|| ((argc <= 3) && !ft_strcmp(argv[1], "-v") && ft_strchr(argv[2], ' '))) //OR 3 WITH -v
+		if (!ft_strcmp(argv[1], "-v"))
 		{
-
-//		ft_printf("c1\n");
-//		ft_printf("argv[1] is: |%s|\n", argv[1]);
-
+			checker_print(argc, argv);
+			return (0);
+		}
+		else if (((argc <= 2) && (ft_strchr(argv[1], ' '))))
+		{
 			temp = ft_strsplit(argv[1], ' ');
-/*
-			i = 0;
-
-			while (i < 4)
-			{
-				ft_printf("temp[%d] is: |%s|\n", i, temp[i]);
-				i++;
-			}
-*/			
 			i = 0;
 			while (temp[i] != NULL)
-			{
-//				ft_printf("temp[%d] is: |%s|\n", i, temp[i]);
 				i++;
-			}
-
-//			ft_printf("i is %d\n", i);
-
 			a = create_stack(i);
 			b = create_stack(i);
-			if (!(convert_args(a, b, temp, (i /*+ 1*/))))
+			if (!(convert_args(a, b, temp, i)))
 			{
 				destroy_stack(a);
 				destroy_stack(b);
@@ -65,38 +51,28 @@ int		main(int argc, char **argv)
 		}
 		else
 		{
-//			ft_printf("c2\n");
-//			ft_printf("argc - 1 is %d\n", argc - 1);
-
 			a = create_stack(argc - 1);	
 			b = create_stack(argc - 1);	
-
-//		ft_printf("argv[1] is: |%s|\n", argv[1]);
-		
-		if (!(convert_args(a, b, argv, argc)))
+			if (!(convert_args(a, b, argv, argc)))
 			{
 				destroy_stack(a);
 				destroy_stack(b);
 				return (-1);
 			}
-			
 		}
-
 		ret = 1;
 		while (ret != 0)
 		{
-
+/*
 			ft_printf("STACK A:\n");
 			print_stack(a);
 			ft_printf("STACK B:\n");
 			print_stack(b);
 			ft_printf("\n");
-
+*/
 			ret = get_next_line(0, &s);
 			if (ret == 1)
 			{
-//				ft_printf("s is |%s|\n", s);
-				
 				if (!ft_strcmp(s, "sa"))
 					swap_ab(a, 'a', 0);
 				else if (!ft_strcmp(s, "sb"))
