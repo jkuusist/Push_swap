636827f90b32853d982d84ec70c706a7e523e9d8
jkuusist <jkuusist@student.hive.fi>
2020-08-17 15:13:09 +0200

    split off some stuff from sort_stack because norme

--- sort_stack.c
+++ sort_stack.c
@@ -13,7 +13,25 @@
 #include "includes/push_swap.h"
 #include "libft/libft.h"
 
-void	sort_stack(t_stack *a, t_stack *b)
+static	void	move_doer(t_stack *a, t_stack *b)
+{
+	int i;
+
+	i = 0;
+	while (b->top > 0)
+		do_least_moves(a, b);
+	push_ab(a, b, 'a', 1);
+	while (i < (int)a->size)
+	{
+		if (stack_peek(a) < a->median)
+			push_ab(a, b, 'b', 1);
+		else
+			rotate_ab(a, 'a', 1);
+		i++;
+	}
+}
+
+void			sort_stack(t_stack *a, t_stack *b)
 {
 	int i;
 
@@ -29,18 +47,7 @@ void	sort_stack(t_stack *a, t_stack *b)
 	}
 	b->smallest = b->arr[get_smallest(b)];
 	b->largest = b->arr[get_largest(b)];
-	while (b->top > 0)
-		do_least_moves(a, b);
-	push_ab(a, b, 'a', 1);
-	i = 0;
-	while (i < (int)a->size)
-	{
-		if (stack_peek(a) < a->median)
-			push_ab(a, b, 'b', 1);
-		else
-			rotate_ab(a, 'a', 1);
-		i++;
-	}
+	move_doer(a, b);
 	b->smallest = b->arr[get_smallest(b)];
 	b->largest = b->arr[get_largest(b)];
 	while (b->top > 0)
