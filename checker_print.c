62a25dfb9efdf2aa50a090ee14fe4f5927996acc
jkuusist <jkuusist@student.hive.fi>
2020-08-18 10:47:14 +0200

    checker_print is now norme-compliant

--- checker_print.c
+++ checker_print.c
@@ -14,7 +14,7 @@
 #include "libft/libft.h"
 #include <stdlib.h>
 
-static	void	convert_split(char **temp, t_stack *a, t_stack *b, int i)
+static	void	convert_to_stacks(char **temp, t_stack *a, t_stack *b, int i)
 {
 	if (!(convert_args(a, b, temp, i)))
 	{
@@ -34,34 +34,43 @@ static	void	convert_split(char **temp, t_stack *a, t_stack *b, int i)
 	free(temp);
 }
 
-void	checker_print(int argc, char **argv)
+static	void	handle_arg(char **argv)
 {
-	char	**temp;
 	t_stack	*a;
 	t_stack	*b;
+	char	**temp;
 	int		i;
+
+	temp = ft_strsplit(argv[2], ' ');
+	i = 0;
+	while (temp[i] != NULL)
+		i++;
+	a = create_stack(i);
+	b = create_stack(i);
+	convert_to_stacks(temp, a, b, i);
+	handle_instructions(a, b, 1);
+	destroy_stack(a);
+	destroy_stack(b);
+}
+
+void			checker_print(int argc, char **argv)
+{
+	t_stack	*a;
+	t_stack	*b;
 
 	if (argc > 1)
 	{
 		if (((argc <= 3) && (ft_strchr(argv[2], ' '))))
-		{
-			temp = ft_strsplit(argv[2], ' ');
-			i = 0;
-			while (temp[i] != NULL)
-				i++;
-			a = create_stack(i);
-			b = create_stack(i);
-			convert_split(temp, a, b, i);
-		}
+			handle_arg(argv);
 		else
 		{
 			a = create_stack(argc - 1);
 			b = create_stack(argc - 1);
 			if (!(convert_args(a, b, argv, argc)))
 				return ;
+			handle_instructions(a, b, 1);
+			destroy_stack(a);
+			destroy_stack(b);
 		}
-		handle_instructions(a, b, 1);
-		destroy_stack(a);
-		destroy_stack(b);
 	}
 }
