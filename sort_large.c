7d3b53027b24385c256b7e9386e8a879f24694cf
jkuusist <jkuusist@student.hive.fi>
2020-08-24 10:18:36 +0200

    switched the rest of sort_large to use push_between as well

--- sort_large.c
+++ sort_large.c
@@ -12,108 +12,49 @@
 
 #include "includes/push_swap.h"
 #include "libft/libft.h"
+#include <limits.h>
 
-static	void	push_or_rot(t_stack *a, t_stack *b)
-{
-	if ((stack_peek(a) < a->second_quint)
-			&& (stack_peek(a) >= a->first_quint))
-		push_ab(a, b, 'b', 1);
-	else
-		rotate_ab(a, 'a', 1);
-}
-
-static	void	do_first_two(t_stack *a, t_stack *b/*, int i*/)
+static	void	do_first_two(t_stack *a, t_stack *b)
 {
 	push_between(a, b, a->third_quint, a->fourth_quint);
-/*
-	while (i < (int)a->size)
-	{
-		if ((stack_peek(a) < a->fourth_quint) && (stack_peek(a) >= a->third_quint))
-			push_ab(a, b, 'b', 1);
-		else
-			rotate_ab(a, 'a', 1);
-		i++;
-	}
-*/
 	b->smallest = b->arr[get_smallest(b)];
 	b->largest = b->arr[get_largest(b)];
 	while (b->top > 0)
 		do_least_moves(a, b);
 	push_ab(a, b, 'a', 1);
-//	i = 0;
-
 	push_between(a, b, a->second_quint, a->third_quint);
-/*
-	while (i < (int)a->size)
-	{
-		if ((stack_peek(a) < a->third_quint)
-			&& (stack_peek(a) >= a->second_quint))
-			push_ab(a, b, 'b', 1);
-		else
-			rotate_ab(a, 'a', 1);
-		i++;
-	}
-*/
 }
 
-static	void	do_next_two(t_stack *a, t_stack *b, int i)
+static	void	do_next_two(t_stack *a, t_stack *b)
 {
 	while (b->top > 0)
 		do_least_moves(a, b);
 	push_ab(a, b, 'a', 1);
-	i = 0;
-	while (i < (int)a->size)
-	{
-		push_or_rot(a, b);
-		i++;
-	}
+	push_between(a, b, a->first_quint, a->second_quint);
 	b->smallest = b->arr[get_smallest(b)];
 	b->largest = b->arr[get_largest(b)];
 	while (b->top > 0)
 		do_least_moves(a, b);
 	push_ab(a, b, 'a', 1);
-	i = 0;
-	while (i < (int)a->size)
-	{
-		if (stack_peek(a) < a->first_quint)
-			push_ab(a, b, 'b', 1);
-		else
-			rotate_ab(a, 'a', 1);
-		i++;
-	}
+	push_between(a, b, a->first_quint, INT_MIN);
 }
 
 void			sort_large(t_stack *a, t_stack *b)
 {
-	int i;
-
-
-	i = 0;
 	get_quints(a);
-	while (i < (int)a->size)
-	{
-		if (stack_peek(a) >= a->fourth_quint)
-			push_ab(a, b, 'b', 1);
-		else
-			rotate_ab(a, 'a', 1);
-		i++;
-	}
+	push_between(a, b, a->fourth_quint, INT_MAX);
 	b->smallest = b->arr[get_smallest(b)];
 	b->largest = b->arr[get_largest(b)];
 	while (b->top > 0)
 		do_least_moves(a, b);
 	push_ab(a, b, 'a', 1);
-	i = 0;
-//	get_quints(a);
-	do_first_two(a, b/*, i*/);
+	do_first_two(a, b);
 	b->smallest = b->arr[get_smallest(b)];
 	b->largest = b->arr[get_largest(b)];
-	i = 0;
-	do_next_two(a, b, i);
+	do_next_two(a, b);
 	b->smallest = b->arr[get_smallest(b)];
 	b->largest = b->arr[get_largest(b)];
 	while (b->top > 0)
 		do_least_moves(a, b);
 	push_ab(a, b, 'a', 1);
-	
 }
