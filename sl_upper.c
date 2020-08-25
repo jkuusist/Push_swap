2bcd72ce8de1e521125d605873d4b6c621a4c85c
jkuusist <jkuusist@student.hive.fi>
2020-08-25 10:30:50 +0200

    finished making sl_upper use push_between. also combined its do_first_two back into the sl_upper function because the reduced amount of lines allowed it

--- sl_upper.c
+++ sl_upper.c
@@ -12,120 +12,41 @@
 
 #include "includes/push_swap.h"
 #include "libft/libft.h"
+#include <limits.h>
 
-/*
-static	void	push_or_rot(t_stack *a, t_stack *b)
+static	void	do_next_two(t_stack *a, t_stack *b)
 {
-	if ((stack_peek(a) < a->seventh_dec)
-			&& (stack_peek(a) >= a->sixth_dec))
-		push_ab(a, b, 'b', 1);
-	else
-		rotate_ab(a, 'a', 1);
-}
-*/
-
-static	void	do_first_two(t_stack *a, t_stack *b/*, int i*/)
-{
-	push_between(a, b, a->eighth_dec, a->ninth_dec);
-/*
-	while (i < (int)a->size)
+	while (b->top > 0)
 	{
-		if ((stack_peek(a) < a->ninth_dec) && (stack_peek(a) >= a->eighth_dec))
-			push_ab(a, b, 'b', 1);
-		else
-			rotate_ab(a, 'a', 1);
-		i++;
+		do_least_moves(a, b);
 	}
-	b->smallest = b->arr[get_smallest(b)];
-*/
+	push_ab(a, b, 'a', 1);
+	push_between(a, b, a->sixth_dec, a->seventh_dec);
+	b->largest = b->arr[get_largest(b)];
+	while (b->top > 0)
+		do_least_moves(a, b);
+	push_ab(a, b, 'a', 1);
+	push_between(a, b, a->fifth_dec, a->sixth_dec);
+}
+
+void			sl_upper(t_stack *a, t_stack *b)
+{
+	get_decs(a);
+	push_between(a, b, a->ninth_dec, INT_MAX);
+	b->largest = b->arr[get_largest(b)];
+	while (b->top > 0)
+		do_least_moves(a, b);
+	push_ab(a, b, 'a', 1);
+	push_between(a, b, a->eighth_dec, a->ninth_dec);
 	b->largest = b->arr[get_largest(b)];
 	while (b->top > 0)
 		do_least_moves(a, b);
 	push_ab(a, b, 'a', 1);
 	push_between(a, b, a->seventh_dec, a->eighth_dec);
-/*
-	i = 0;
-	while (i < (int)a->size)
-	{
-		if ((stack_peek(a) < a->eighth_dec)
-			&& (stack_peek(a) >= a->seventh_dec))
-			push_ab(a, b, 'b', 1);
-		else
-			rotate_ab(a, 'a', 1);
-		i++;
-	}
-*/
-}
-
-static	void	do_next_two(t_stack *a, t_stack *b/*, int i*/)
-{
-	while (b->top > 0)
-	{
-		do_least_moves(a, b);
-	}
-	push_ab(a, b, 'a', 1);
-
-	push_between(a, b, a->sixth_dec, a->seventh_dec);
-/*
-	i = 0;
-	while (i < (int)a->size)
-	{
-		push_or_rot(a, b);
-		i++;
-	}
-	b->smallest = b->arr[get_smallest(b)];
-*/
+	b->largest = b->arr[get_largest(b)];
+	do_next_two(a, b);
 	b->largest = b->arr[get_largest(b)];
 	while (b->top > 0)
 		do_least_moves(a, b);
-	push_ab(a, b, 'a', 1);
-
-	push_between(a, b, a->fifth_dec, a->sixth_dec);
-/*
-	i = 0;
-	while (i < (int)a->size)
-	{
-		if ((stack_peek(a) < a->sixth_dec) && (stack_peek(a) >= a->fifth_dec))
-			push_ab(a, b, 'b', 1);
-		else
-			rotate_ab(a, 'a', 1);
-		i++;
-	}
-*/
-}
-
-void			sl_upper(t_stack *a, t_stack *b)
-{
-	int i;
-
-	i = 0;
-	get_decs(a);
-	while (i < (int)a->size)
-	{
-		if (stack_peek(a) >= a->ninth_dec)
-			push_ab(a, b, 'b', 1);
-		else
-			rotate_ab(a, 'a', 1);
-		i++;
-	}
-	b->smallest = b->arr[get_smallest(b)];
-	b->largest = b->arr[get_largest(b)];
-	while (b->top > 0)
-	{
-		do_least_moves(a, b);
-	}
-	push_ab(a, b, 'a', 1);
-	i = 0;
-	do_first_two(a, b/*, i*/);
-	b->largest = b->arr[get_largest(b)];
-//	i = 0;
-	do_next_two(a, b/*, i*/);
-	b->smallest = b->arr[get_smallest(b)];
-	b->largest = b->arr[get_largest(b)];
-	while (b->top > 0)
-	{
-
-		do_least_moves(a, b);
-	}
 	push_ab(a, b, 'a', 1);
 }
