8723f3ed6aa0ceb29625950acd8d43377711b683
jkuusist <jkuusist@student.hive.fi>
2020-09-03 09:44:57 +0200

    split off stuff from push_between to get to 25 lines or less

--- push_between.c
+++ push_between.c
@@ -15,47 +15,54 @@
 
 #include "libft/libft.h"
 
-void	push_between(t_stack *a, t_stack *b, int lower, int higher)
+static int		get_first(t_stack *a, int lower, int higher)
 {
 	int i;
-	int j;
+
+	i = a->top;
+	while (i >= 0)
+	{
+		if ((a->arr[i] < higher) && (a->arr[i] >= lower))
+			return (i);
+		i--;
+	}
+	return (-1);
+}
+
+static int		get_second(t_stack *a, int lower, int higher)
+{
+	int i;
+
+	i = 0;
+	while (i <= a->top)
+	{
+		if ((a->arr[i] < higher) && (a->arr[i] >= lower))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+void			push_between(t_stack *a, t_stack *b, int lower, int higher)
+{
+	int i;
 	int first_i;
 	int second_i;
 
-	i = 0;	
+	i = 0;
 	while (is_in_stack(a, lower, higher))
 	{
-		j = a->top;
-		first_i = -1;
-		second_i = -1;
-		while (j >= 0)
-		{
-			if ((a->arr[j] < higher) && (a->arr[j] >= lower))
-			{
-				first_i = j; 
-				break;
-			}
-			j--;
-		}
-		j = 0;
-		while (j <= a->top)
-		{
-			if ((a->arr[j] < higher) && (a->arr[j] >= lower))
-			{
-				second_i = j;
-				break;
-			}
-			j++;
-		}
+		first_i = get_first(a, lower, higher);
+		second_i = get_second(a, lower, higher);
 		if ((second_i != -1) && ((second_i + 1) < (a->top - first_i)))
 		{
-			while (!((stack_peek(a) < higher ) && (stack_peek(a) >= lower)))
+			while (!((stack_peek(a) < higher) && (stack_peek(a) >= lower)))
 				rev_rotate_ab(a, 'a', 1);
 			push_ab(a, b, 'b', 1);
 		}
 		else
 		{
-			while (!((stack_peek(a) < higher ) && (stack_peek(a) >= lower)))
+			while (!((stack_peek(a) < higher) && (stack_peek(a) >= lower)))
 				rotate_ab(a, 'a', 1);
 			push_ab(a, b, 'b', 1);
 		}
